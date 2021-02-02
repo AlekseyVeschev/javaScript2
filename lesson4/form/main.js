764c6b12df621cdfc55b85a995edb09417e623b9
Aleksey <aleksey.veshev@mail.ru>
2021-02-02 23:53:31 +0300

    lesson4

---
+++ lesson4/form/main.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form")
+const inputName = document.querySelector(".name")
+const inputTel = document.querySelector(".tel")
+const inputEmail = document.querySelector(".email")
+const button = document.querySelector("button")
+const onSubmit = (event) => {
+    // event.preventDefault()
+    console.log("ffgfg")
+}
+button.addEventListener("submit", onSubmit)  
