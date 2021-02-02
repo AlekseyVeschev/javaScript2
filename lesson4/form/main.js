78df4f60248ef700c5785357f08e354838418bad
Aleksey <aleksey.veshev@mail.ru>
2021-02-03 02:43:03 +0300

    add form

--- lesson4/form/main.js
+++ lesson4/form/main.js
@@ -4,7 +4,18 @@ const inputTel = document.querySelector(".tel")
 const inputEmail = document.querySelector(".email")
 const button = document.querySelector("button")
 const onSubmit = (event) => {
-    // event.preventDefault()
-    console.log("ffgfg")
+    event.preventDefault()
+    if (event.target[0].value.length > 10 || !/[a-z]/i.test(event.target[0].value)) {
+        inputName.classList.add("error")
+        inputName.value = "неправильно заполнено имя"
+    }
+    if (!/\(\d{3}\)\d{3}\-\d{4}/.test(event.target[1].value)) {
+        inputTel.classList.add("error")
+        inputTel.value = "неправильно заполнено"
+    }
+    if (!/[a-z\.\-]{6,7}@[a-z]{4}\.[a-z]{2}/i.test(event.target[2].value)) {
+        inputEmail.classList.add("error")
+        inputEmail.value = "неправильно заполнен email"
+    }
 }
-button.addEventListener("submit", onSubmit)  
+form.addEventListener("submit", onSubmit)
